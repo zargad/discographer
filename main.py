28d159550ef2aad924171a18b2eeb8f39edece4f
Thompson Leonard <t0pl0l250@gmail.com>
2024-07-20 02:21:25 +0300

    fix bugs

--- main.py
+++ main.py
@@ -1,39 +1,64 @@
 #!/usr/bin/env python3
 """Print out a discography of an artist."""
+from datetime import datetime
 from time import sleep
 import discogs_api
 
 
 def main():
-    d = discogs_api.Client('ExampleApplication/1.0')
-    artist = d.artist(input('artist id: '))
-    print('#', artist.name)
-    months = {
-        '01': 'Jan',
-        '02': 'Feb',
-        '03': 'Mar',
-        '04': 'Apr',
-        '05': 'May',
-        '06': 'Jun',
-        '07': 'Jul',
-        '08': 'Aug',
-        '09': 'Sep',
-        '10': 'Oct',
-        '11': 'Nov',
-        '12': 'Dec',
-    }
+    client = discogs_api.Client('ExampleApplication/1.0')
+    artist = client.artist(input('artist id: '))
+    releases = get_releases(client, artist)
+    releases.sort(key=lambda r: r[1])
+    print_releases(artist.name, releases)
+
+
+def print_releases(artist_name, releases):
+    months = [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec',
+    ]
+    print('#', artist_name)
     year = None
-    for release in artist.releases:
-        if isinstance(release, discogs_api.Master):
-            release = d.release(release.versions[0].id)
-        date = release.released.split('-')
-        if year != date[0]:
-            year = date[0]
+    for (title, date) in releases:
+        if year != date.year:
+            year = date.year
             print()
             print('##', year)
             print()
-        print(release.title, '-', months[date[1]], date[2])
+        print(title, '-', months[date.month - 1], date.day)
+
+
+def get_releases(client, artist):
+    releases = []
+    for i, release in enumerate(artist.releases, start=1):
+        print(i, '/', len(artist.releases))
+        if isinstance(release, discogs_api.Master):
+            release = client.release(release.versions[0].id)
+        title = release.title
+        print(release.released)
+        date = get_date(release.released)
+        releases.append((title, date))
         sleep(10)
+    return releases
+
+
+def get_date(raw_date):
+    date = list(map(int, raw_date.split('-')))
+    date += [0] * (3 - len(date))
+    date = map(lambda n: 1 if n == 0 else n, date)
+    date = datetime(*date)
+    return date
 
 
 if __name__ == '__main__':
